b99461eb208074cb9a2b06d55235ba329448f196
YourAKShaw <hitme@yourakshaw.com>
2023-11-17 14:00:39 +0530

    feat: :sparkles: add my solution to excersie-1

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const setEditAction = () => {
+    setIsEditing(!isEditing);
+  };
+    let element = <span className="player-name">{name}</span>;
+
+  if (isEditing) {
+    element = <input></input>
+  }
+
   return (
     <li>
       <span className="player-info">
-        <span className="player-name">{name}</span>
+        {element}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={setEditAction}>Edit</button>
     </li>
   );
 }
