78c5ab7aec671c3293e709d27ae798a2c2e614b6
YourAKShaw <hitme@yourakshaw.com>
2023-11-17 13:26:44 +0530

    feat: :sparkles: add initial structure to App.jsx using Player.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import Player from "./components/Player.jsx";
+
 function App() {
-  return <p>Coming soon...</p>;
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player1" symbol="X" />
+          <Player name="Player2" symbol="O" />
+        </ol>
+        GAME BOARD
+      </div>
+      LOG
+    </main>
+  );
 }
 
 export default App;
