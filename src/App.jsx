3fe6ac974ca7905ac64d3dc5bcd5bdd2542685e7
YourAKShaw <hitme@yourakshaw.com>
2023-11-17 15:59:15 +0530

    feat: :sparkles: add initial GameBoard grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Player from "./components/Player.jsx";
+import GameBoard from "./components/GameBoard.jsx";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
           <Player initialName="Player1" symbol="X" />
           <Player initialName="Player2" symbol="O" />
         </ol>
-        GAME BOARD
+        <GameBoard />
       </div>
       LOG
     </main>
---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,23 @@
+const initialGameBoardd = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard() {
+  return (
+    <ol id="game-board">
+      {initialGameBoardd.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button>{playerSymbol}</button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
